3e88eaaca4918540378af0ee63a5a4ad55a14489
SSithub <57548627+SSithub@users.noreply.github.com>
2021-04-25 18:12:49 -0400

    API functions

--- api.py
+++ api.py
@@ -1,9 +1,25 @@
 import os
 import requests
 
+# reading in the api key from the apikey.txt in the directory with this file
 cd = os.path.dirname(os.path.realpath(__file__))
 apikeypath = cd + "/apikey.txt"
-
 apikeyfile = open(apikeypath)
 apikey = apikeyfile.readline()[0:-1]
-print(apikey)
+
+# api functions
+apiurl = "https://api.bscscan.com/api?module="
+
+
+def request(url):
+    response = requests.get(apiurl + url)
+    if response.status_code != 200:
+        print("Error", response.status_code, response.json())
+    return response
+
+
+def post(url, data):
+    response = requests.post(apiurl + url, data=data)
+    if response.status_code != 200:
+        print("Error", response.status_code, response.json())
+    return response
